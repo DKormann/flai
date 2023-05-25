4f78f64b3da420773c2ced4d039155022d071f84
Florian <45694132+flo-bit@users.noreply.github.com>
2023-05-25 14:23:46 +0200

    test

--- src/routes/bubble.svelte
+++ src/routes/bubble.svelte
@@ -1,23 +1,21 @@
-<script lang='ts'>
+<script lang="ts">
+  export var data: { translation: string; original: string };
 
-    export var data:{translation:string, original:string}
+  var opened = false;
 
-    var opened = false
+  function toggle() {
+    console.log("toggle");
 
-    function toggle(){
-        console.log("toggle");
-        
-        opened = !opened
-    }
-
+    opened = !opened;
+  }
 </script>
 
-<button class="text-primary" on:click={toggle} >
-    
+<button class="text-primary" on:click={toggle}>
+  {#if opened}
+    <div class="tooltip" data-tip={data.original}>
+      {data.translation}
+    </div>
+  {:else}
     {data.translation}
-
-    {#if opened}
-        <span class="bg-primary text-white rounded-xl">&nbsp;{data.original}&nbsp;</span>
-    {/if}
-
+  {/if}
 </button>
