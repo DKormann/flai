c6bc3d93d2752ae597a39e57a51d33cf0282316a
Florian <45694132+flo-bit@users.noreply.github.com>
2023-05-26 10:45:49 +0200

    remove api key

--- src/routes/gpt_request.ts
+++ src/routes/gpt_request.ts
@@ -1,29 +1,27 @@
-const apikey = "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"
+const apikey = "";
 
-export async function gpt_request(prompt:string){
+export async function gpt_request(prompt: string) {
+  const url = "https://api.openai.com/v1/engines/text-davinci-003/completions";
 
-    const url = "https://api.openai.com/v1/engines/text-davinci-003/completions"
-    
-    let resp = await fetch(url, {
-        method: "POST",
-        headers: {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + apikey
-        },
-        body: JSON.stringify({
-        prompt: prompt,
-        max_tokens: 100,
-        temperature: 0.7,
-        top_p: 1,
-        n: 1,
-        stream: false,
-        logprobs: null,
-        stop: ["\n"]
-        })
-    })
-    
-    let data = await resp.json()
-    
-    return data.choices[0].text
-    
+  let resp = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + apikey,
+    },
+    body: JSON.stringify({
+      prompt: prompt,
+      max_tokens: 100,
+      temperature: 0.7,
+      top_p: 1,
+      n: 1,
+      stream: false,
+      logprobs: null,
+      stop: ["\n"],
+    }),
+  });
+
+  let data = await resp.json();
+
+  return data.choices[0].text;
 }
