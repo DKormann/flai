1ce7d5a0e678500ced9b3cc653828d563d3b942e
Dominik Kormann <dominikormann@outlook.com>
2023-06-08 13:19:54 +0200

    see sentence

--- src/routes/bubble.svelte
+++ src/routes/bubble.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
   import { onDestroy } from "svelte";
 
-  export let data: { translation: string; original: string };
+  export let data: { translation: string; original: string ; active:boolean};
   export let index: number;
   export let openTooltipIndex: number;
   export let toggleTooltip: (index: number) => void;
   export let fail:()=>void;
+
 
   import { fade } from "svelte/transition";
 
@@ -16,17 +17,14 @@
   }
 </script>
 
-<button class="text-primary relative" on:click={on_click}>
+<button class={data.active?"text-primary": "text-[#eea]" +" relative"} on:click={on_click}>
   
   {#if openTooltipIndex === index}
     <span class="text-secondary">
         {data.translation}
     </span>
-    <div
-    class="absolute text-white mt-1 p-1 px-4 z-10 bg-gray-700 bg-opacity-100 rounded shadow-md"
-
-    >
-    {data.original}
+    <div class="absolute text-white mt-1 p-1 px-4 z-10 bg-gray-700 bg-opacity-100 rounded shadow-md">
+      {data.original}
     </div>
   {:else}
     {data.translation}
